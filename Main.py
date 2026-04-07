fdb73b5fcee606bdd903fe0c9339f80781983b30
Yash Dahiwlikar <83547433+YashDahiwlikar914@users.noreply.github.com>
2026-04-07 18:30:36 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,21 @@
 import math
+import requests
+import hashlib
 
-print("\n<----- PassAudit Tool ----->\n")
+def checkBreach(password):
+    hash = hashlib.sha1(password.encode('utf-8')).hexdigest.upper()
+    prefix, suffix = hash[:5], hash[5:]
+    
+    url = f"https://api.pwnedpasswords.com/range/{prefix}"
+    response = requests.get(url)
 
-print("Choose task to do -\n"
-      "Type 0 to do Policy Analysis\n"
-      "Type 1 to do Password Rating\n"
-      "Type 2 to do Password Rating(Bulk)\n"
-      "Type 3 to do Password Entropy Calculation")
-choice = str(input("-> "))
+    if response.status_code == 200:
+        
+    
 
-entropy = 0
 
-if choice == '0':
+def policyAnalysis():
+    global entropy
     l = int(input("Enter minimum length : "))
     upperCase = str(input("Includes uppercase (Y/N) : ")).lower()
     lowerCase = str(input("Includes lowercase (Y/N) : ")).lower()
@@ -94,8 +98,64 @@ if choice == '0':
           "Avoid security questions\n"
           "Avoid dictionary words and repetition of characters\n")
 
+def passwordRating():
+    global entropy
+    password = str(input("Enter the password : "))
+    l = len(password)
+
+    n = 0
+    if any(c.isupper() for c in password): n += 26
+    if any(c.islower() for c in password): n += 26
+    if any(c.isdigit() for c in password): n += 10
+    if any(c in "!@#$%^&*()-_=+[]{}|;:',.<>?/`~" for c in password): n += 32
+
+    score = 0
+
+    if l < 8:
+        pass
+    elif 8 <= l < 12:
+        score += 3
+    elif 16 > l >= 12:
+        score += 5
+    else:
+        score += 5
+
+    if n <= 26:
+        pass
+    elif 26 < n <= 52:
+        score += 1
+    elif 52 < n <= 62:
+        score += 2
+    elif n > 62:
+        score += 3
+
+    breachCount = checkBreach(password)
+
+    if breachCount > 0:
+        score -= 3
+    else:
+        score += 2
+    
+    
+    
+    
+    
+
+print("\n<----- PassAudit Tool ----->\n")
+
+print("Choose task to do -\n"
+      "Type 0 to do Policy Analysis\n"
+      "Type 1 to do Password Rating\n"
+      "Type 2 to do Password Rating(Bulk)\n"
+      "Type 3 to do Password Entropy Calculation")
+choice = str(input("-> "))
+
+entropy = 0
+
+if choice == '0':
+    policyAnalysis()
 elif choice == '1':
-    print("Feature Coming Soon!")
+    passwordRating()
 elif choice == '2':
     print("Feature Coming Soon!")
 elif choice == '3':
